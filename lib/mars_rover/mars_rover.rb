d12e8e82dd086a06ff732b41036b0e96b0f1c1e9
Shane Kelly <shane.sparky.kelly@gmail.com>
2020-07-06 19:55:59 +0200

    feat: Add a tested MarsRover class

---
+++ lib/mars_rover/mars_rover.rb
@@ -0,0 +1,80 @@
+module MarsRover
+  class MarsRover
+    attr_accessor :position, :bearing
+
+    def initialize(position: default_position, bearing: default_bearing)
+      @position = position
+      @bearing  = bearing
+    end
+
+    def x
+      position[:x]
+    end
+
+    def y
+      position[:y]
+    end
+
+    def rotate(direction)
+      if left_aliases.include? direction.to_s.downcase
+        rotate_left
+      elsif right_aliases.include? direction.to_s.downcase
+        rotate_right
+      else
+        raise ArgumentError.new("Incorrect direction specified. The correct inputs are: #{direction_aliases}")
+      end
+    end
+
+    def move_forward
+      @position =
+      case @bearing
+        when 'N' then { x: x,     y: y + 1 }
+        when 'E' then { x: x + 1, y: y     }
+        when 'S' then { x: x,     y: y - 1 }
+        when 'W' then { x: x - 1, y: y     } 
+      end
+    end
+
+    private
+
+    def default_position
+      { x: 0, y: 0 }
+    end
+
+    def default_bearing
+      'N'
+    end
+
+    def rotate_right
+      @bearing =
+      case @bearing
+        when 'N' then 'E'
+        when 'E' then 'S'
+        when 'S' then 'W'
+        when 'W' then 'N'
+      end
+    end
+
+    def rotate_left
+      @bearing =
+      case @bearing
+        when 'N' then 'W'
+        when 'W' then 'S'
+        when 'S' then 'E'
+        when 'E' then 'N'
+      end
+    end
+
+    def left_aliases
+      %w{ l left anticlockwise anti-clockwise -90 }
+    end
+
+    def right_aliases
+      %w{ r right clockwise 90 }
+    end
+
+    def direction_aliases
+      left_aliases + right_aliases
+    end
+  end
+end
---
+++ spec/mars_rover/mars_rover_spec.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+RSpec.describe MarsRover::MarsRover do
+  let(:rover) { described_class.new(position: { x: 1, y: 3 }, bearing: 'N') }
+
+  it 'has a position' do
+    expect(rover.position).to eq( { x: 1, y: 3 } )
+  end
+
+  it 'has a bearing' do
+    expect(rover.bearing).to eq('N')
+  end
+
+  context 'when rotating' do
+    it 'can rotate left' do
+      expect { rover.rotate('L') }.to change { rover.bearing }.from('N').to('W')
+    end
+
+    it 'can rotate right' do
+      expect { rover.rotate('R') }.to change { rover.bearing }.from('N').to('E')
+    end
+
+    it 'raises an error when an invalid direction is offered' do
+      expect { rover.rotate('nope')}.to raise_error(ArgumentError)
+    end
+  end
+
+  context 'when moving forward' do
+    it 'only moves forward one square' do
+      expect { rover.move_forward }.to change { rover.position } \
+      .from({ x: 1, y: 3 })
+      .to(  { x: 1, y: 4 })
+    end
+
+    it 'does not change bearing' do
+      expect { rover.move_forward }.to_not change { rover.bearing }
+    end
+  end
+
+  # I'll probably move the below tests to the MarsRover::Map spec
+  context "when consulting a map" do
+    it 'moves forward when the square is empty' do
+      rover.can_move_forward?(map: MarsRover::Map.new(upper_right_corner: { x: 5, y: 5 }))
+    end
+
+    it 'does not move forward when the square is not on the grid'
+    it 'does not move forward if there is another rover on the square'
+  end
+end
